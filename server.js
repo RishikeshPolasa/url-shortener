930f9eacf957c4cafe1a5f84647a0547f5334ff1
RishikeshPolasa <rishipolasa@gmail.com>
2021-07-31 08:32:15 +0530

    added shortUrl.js

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./model/shortUrl");
+
 const app = express();
 
-app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false })); //to access the parameters from req.body
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  console.log(req.body.fullUrl);
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 5000);
